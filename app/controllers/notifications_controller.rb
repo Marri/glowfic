c1b82789cbafd1894b48892f4a555de1ca62a6db
teceler <19716939+teceler@users.noreply.github.com>
2021-07-01 13:32:34 -0500

    Allow marking notifications read and deleting them

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,4 +4,22 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.notifications
   end
+
+  def mark
+    notifications = Notification.where(id: params[:marked_ids], user: current_user)
+
+    if params[:commit] == "Mark Read"
+      notifications.each { |notif| notif.update(unread: false, read_at: notif.read_at || Time.zone.now) }
+    elsif params[:commit] == "Mark Unread"
+      notifications.each { |notif| notif.update(unread: true) }
+    elsif params[:commit] == "Delete"
+      notifications.destroy_all
+    else
+      flash[:error] = "Could not perform unknown action."
+      redirect_to notification_path and return
+    end
+
+    flash[:success] = "Messages updated"
+    redirect_to notifications_path
+  end
 end
