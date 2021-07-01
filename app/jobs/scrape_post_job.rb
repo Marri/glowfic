a29a729dcff731faa4869d011b1ffc1d817fc267
teceler <19716939+teceler@users.noreply.github.com>
2021-06-30 23:24:24 -0500

    Remember import errors

--- app/jobs/scrape_post_job.rb
+++ app/jobs/scrape_post_job.rb
@@ -10,15 +10,13 @@ class ScrapePostJob < ApplicationJob
 
   def self.notify_exception(exception, url, board_id, section_id, status, threaded, importer_id)
     Resque.logger.warn "Failed to import #{url}: #{exception.message}"
-    if (importer = User.find_by_id(importer_id))
-      Notification.notify_user(importer, :import_fail)
+    importer = User.find_by_id(importer_id)
+    super unless importer
+    if exception.is_a?(AlreadyImportedError)
+      Notification.notify_user(importer, :import_fail, post: Post.find_by(id: exception.post_id))
+    else
+      Notification.notify_user(importer, :import_fail, error: exception.message)
     end
     super
-  end
-
-  def self.view_post(post_id)
-    host = ENV['DOMAIN_NAME'] || 'localhost:3000'
-    url = Rails.application.routes.url_helpers.post_url(post_id, host: host, protocol: 'https')
-    "<a href='#{url}'>View it here</a>."
   end
 end
