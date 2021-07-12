252084f5281b03badf7a7809c9be5e29eba1bce8
teceler <19716939+teceler@users.noreply.github.com>
2021-07-12 16:48:43 -0500

    Create hash matcher

--- spec/support/spec_test_helper.rb
+++ spec/support/spec_test_helper.rb
@@ -6,6 +6,32 @@ module SpecTestHelper
   def login
     login_as(create(:user))
   end
+
+  RSpec::Matchers.define :match_hash do |expected|
+    match do |actual|
+      return false unless expected.is_a?(Hash) && actual.is_a?(Hash)
+      @actual = transform(actual)
+      @expected = transform(expected)
+      @actual.eql?(@expected)
+    end
+
+    def transform(parameter)
+      parameter.transform_keys!(&:to_s)
+      parameter.transform_values! do |v|
+        if v.is_a?(ActiveRecord::Relation)
+          v.to_a
+        elsif v.is_a?(Array)
+          v
+        else
+          v.to_s
+        end
+      end
+      parameter.sort_by{ |k, _v| k }.to_h
+    end
+
+    diffable
+    attr_reader :actual, :expected
+  end
 end
 
 def stub_fixture(url, filename)
